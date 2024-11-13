73912cbe5ae9ec81f7b19b2f47db72e6c31033e0
Ben Whitehead <ben.pi.whitehead@gmail.com>
2024-11-13 18:14:04 +0000

    Form Check SM
    This commit adds informative errors for various issues with the format of the file.
    This commit does not check whether the preferences lists contents refer to real people in the instance, only that they are all integers.

--- src/algmatch/stableMatchings/stableMarriageProblem/fileReader.py
+++ src/algmatch/stableMatchings/stableMarriageProblem/fileReader.py
@@ -3,36 +3,64 @@ Class to read in a file of preferences for the Stable Marriage Problem stable ma
 """
 
 from algmatch.abstractClasses.abstractReader import AbstractReader
+from algmatch.abstractClasses.abstractReader import FileError
 
 
 class FileReader(AbstractReader):
     def __init__(self, filename: str) -> None:
         super().__init__(filename)
-        self._read_data()
+        self._read_data()   
 
     def _read_data(self) -> None:
+
         self.no_men = 0
         self.no_women = 0
         self.men = {}                
         self.women = {}
+        cur_line = 1
         
         with open(self.data, 'r') as file:
             file = file.read().splitlines()
 
-        self.no_men, self.no_women = map(int, file[0].split())
+        try:
+            self.no_men, self.no_women = map(int, file[0].split())
+        except ValueError:
+            raise FileError(cur_line, "Participant Quantities Misformatted")
 
         # build men dictionary
         for elt in file[1:self.no_men+1]:
+            cur_line += 1
             entry = elt.split()
+
+            if not entry or not entry[0].isdigit():
+                raise FileError(cur_line, "Man ID Misformatted")
             man = f"m{entry[0]}"
+            if man in self.men:
+                raise FileError(cur_line, "Repeated Man ID")
+
+            for i in entry[1:]:
+                if not i.isdigit():
+                    raise FileError(cur_line, "Man preference List Misformatted")
             preferences = [f"w{i}" for i in entry[1:]]
 
             self.men[man] = {"list": preferences, "rank": {}}
 
         # build women dictionary
         for elt in file[self.no_men+1:self.no_men+self.no_women+1]:
+            cur_line += 1
+            print(cur_line)
             entry = elt.split()
+            print(entry)
+
+            if not entry or not entry[0].isdigit():
+                raise FileError(cur_line, "Woman ID Misformatted")
             woman = f"w{entry[0]}"
+            if woman in self.women:
+                raise FileError(cur_line, "Repeated Woman ID")
+
+            for i in entry[1:]:
+                if not i.isdigit():
+                    raise FileError(cur_line, "Woman preference List Misformatted")
             preferences = [f"m{i}" for i in entry[1:]]
 
             self.women[woman] = {"list": preferences, "rank": {}}
@@ -53,3 +81,7 @@ class FileReader(AbstractReader):
                     acceptable_w_prefs.append(m)
             self.women[w]["list"] = acceptable_w_prefs
             self.women[w]["rank"] = {man: idx for idx, man in enumerate(acceptable_w_prefs)}
+
+f = FileReader(filename="src/algmatch/example.txt")
+print(f.men)
+print(f.women)
