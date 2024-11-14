614063c51a13d6586a59a3e043cb9ff33ff4760a
BenOnTheBoard <ben.pi.whitehead@gmail.com>
2024-11-14 20:53:51 +0000

    HR Readers basic errors setup

--- src/algmatch/stableMatchings/hospitalResidentsProblem/dictionaryReader.py
+++ src/algmatch/stableMatchings/hospitalResidentsProblem/dictionaryReader.py
@@ -3,7 +3,7 @@ Class to read in a dictionary of preferences for the Hospital/Residents Problem 
 """
 
 from algmatch.abstractClasses.abstractReader import AbstractReader
-
+from algmatch.abstractClasses.abstractReader import ReaderError
 
 class DictionaryReader(AbstractReader):
     def __init__(self, dictionary: dict) -> None:
@@ -18,32 +18,34 @@ class DictionaryReader(AbstractReader):
             match key:
                 case "residents":
                     for k, v in value.items():
+                        if type(k) is not int:
+                            raise ReaderError(f"resident {k}", "Resident ID misformatted")
                         resident = f"r{k}"
+                        if resident in self.residents:
+                            raise ReaderError(f"resident {k}", "Repeated resident ID")
+                        
+                        for i in v:
+                            if type(i) is not int:
+                                raise ReaderError(f"resident {k}", "Resident preference list misformatted; {i} is not int")
                         preferences = [f"h{i}" for i in v]
 
                         self.residents[resident] = {"list": preferences, "rank": {}}
 
                 case "hospitals":
                     for k, v in value.items():
+                        if type(k) is not int:
+                            raise ReaderError(f"hospital {k}", "Hospital ID misformatted")
                         hospital = f"h{k}"
+                        if hospital in self.hospitals:
+                            raise ReaderError(f"hospital {k}", "Repeated hospital ID")
+                        
+                        if type(v["capacity"]) is not int:
+                            raise ReaderError(f"hospital {k}", "Capacity is not int")
                         capacity = v["capacity"]
+                        
+                        for i in v:
+                            if type(i) is not int:
+                                raise ReaderError(f"hospital {k}", "Hospital preference list misformatted; {i} is not int")  
                         preferences = [f"r{i}" for i in v["preferences"]]
 
                         self.hospitals[hospital] = {"capacity": capacity, "list": preferences, "rank": {}}
-
-        #remove unacceptable pairs
-        for r, r_prefs in self.residents.items():
-            acceptable_r_prefs = []
-            for h in r_prefs["list"]:
-                if r in self.hospitals[h]["list"]:
-                    acceptable_r_prefs.append(h)
-            self.residents[r]["list"] = acceptable_r_prefs
-            self.residents[r]["rank"] = {hospital: idx for idx, hospital in enumerate(acceptable_r_prefs)}
-            
-        for h, h_prefs in self.hospitals.items():
-            acceptable_h_prefs = []
-            for r in h_prefs["list"]:
-                if h in self.residents[r]["list"]:
-                    acceptable_h_prefs.append(r)
-            self.hospitals[h]["list"] = acceptable_h_prefs
-            self.hospitals[h]["rank"] = {resident: idx for idx, resident in enumerate(acceptable_h_prefs)}
--- src/algmatch/stableMatchings/hospitalResidentsProblem/fileReader.py
+++ src/algmatch/stableMatchings/hospitalResidentsProblem/fileReader.py
@@ -3,6 +3,7 @@ Class to read in a file of preferences for the Hospitals/Residents Problem stabl
 """
 
 from algmatch.abstractClasses.abstractReader import AbstractReader
+from algmatch.abstractClasses.abstractReader import ReaderError
 
 class FileReader(AbstractReader):
     def __init__(self, filename: str) -> None:
@@ -14,43 +15,46 @@ class FileReader(AbstractReader):
         self.no_hospitals = 0
         self.residents = {}                
         self.hospitals = {}
+        cur_line = 1
         
         with open(self.data, 'r') as file:
             file = file.read().splitlines()
 
-        self.no_residents, self.no_hospitals = map(int, file[0].split())
+        try:
+            self.no_residents, self.no_hospitals = map(int, file[0].split())
+        except ValueError:
+            raise ReaderError(f"line {cur_line}", "Participant Quantities Misformatted")
 
         # build residents dictionary
         for elt in file[1:self.no_residents+1]:
+            cur_line += 1
             entry = elt.split()
+
+            if not entry or not entry[0].isdigit():
+                raise ReaderError(f"line {cur_line}", "Resident ID misformatted")
             resident = f"r{entry[0]}"
+            if resident in self.residents:
+                raise ReaderError(f"line {cur_line}", "Repeated resident ID")
+
+            for i in entry[1:]:
+                if not i.isdigit():
+                    raise ReaderError(f"line {cur_line}", "Resident preference list misformatted; {i} is not int>0")
             preferences = [f"h{i}" for i in entry[1:]]
 
             self.residents[resident] = {"list": preferences, "rank": {}}
 
         # build hospitals dictionary
         for elt in file[self.no_residents+1:self.no_residents+self.no_hospitals+1]:
+            cur_line += 1
             entry = elt.split()
+
+            if not entry or not entry[0].isdigit():
+                raise ReaderError(f"line {cur_line}", "Hospital ID misformatted")
             hospital = f"h{entry[0]}"
+            if hospital in self.hospitals:
+                raise ReaderError(f"line {cur_line}", "Repeated hospital ID")
+
             capacity = int(entry[1])
             preferences = [f"r{i}" for i in entry[2:]]
 
             self.hospitals[hospital] = {"capacity": capacity, "list": preferences, "rank": {}}
-
-        #remove unacceptable pairs
-        for r, r_prefs in self.residents.items():
-            acceptable_r_prefs = []
-            for h in r_prefs["list"]:
-                if r in self.hospitals[h]["list"]:
-                    acceptable_r_prefs.append(h)
-            self.residents[r]["list"] = acceptable_r_prefs
-            self.residents[r]["rank"] = {hospital: idx for idx, hospital in enumerate(acceptable_r_prefs)}
-            
-        for h, h_prefs in self.hospitals.items():
-            acceptable_h_prefs = []
-            for r in h_prefs["list"]:
-                if h in self.residents[r]["list"]:
-                    acceptable_h_prefs.append(r)
-            self.hospitals[h]["list"] = acceptable_h_prefs
-            self.hospitals[h]["rank"] = {resident: idx for idx, resident in enumerate(acceptable_h_prefs)}
-
