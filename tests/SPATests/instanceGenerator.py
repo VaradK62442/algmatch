705f70560ab0aea0116cf614b2da290e3288072f
VaradK62442 <varadkulk123@gmail.com>
2024-11-25 20:06:10 +0000

    rename SPA instance generator to match HR and SM

--- tests/SPATests/instanceGenerator.py
+++ tests/SPATests/instanceGenerator.py
@@ -1,7 +1,7 @@
 import random
 from math import ceil
 
-class SPAS:
+class SPAInstanceGenerator:
     def __init__(self, students, lower_bound, upper_bound):
         if type(students) is not int or students <= 0:
             raise ValueError("number of residents must be a postive integer")
