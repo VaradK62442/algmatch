5ef25d765971a1a939fd0dd987ec0dd8853f0649
BenOnTheBoard <ben.pi.whitehead@gmail.com>
2024-12-02 02:47:11 +0000

    Regex-Style File Reader SMT

--- src/algmatch/stableMatchings/stableMarriageProblem/ties/fileReader.py
+++ src/algmatch/stableMatchings/stableMarriageProblem/ties/fileReader.py
@@ -1,21 +1,56 @@
 """
 Class to read in a file of preferences for the Stable Marriage Problem stable matching algorithm.
 """
+from re import findall
 
 from algmatch.abstractClasses.abstractReader import AbstractReader
-from algmatch.errors.ReaderErrors import ParticipantQuantityError, IDMisformatError, RepeatIDError, PrefListMisformatError
+from algmatch.errors.ReaderErrors import (
+    IDMisformatError,
+    NestedTiesError,
+    ParticipantQuantityError,
+    RepeatIDError,
+    UnclosedTieError,
+    UnopenedTieError
+)
 
 class FileReader(AbstractReader):
     def __init__(self, filename: str) -> None:
         super().__init__(filename)
         self._read_data()
 
+    def regex_split(self,line):
+        return findall(r"\d+|[\(\)]", line)
+
+    def _scan_preference_tokens(self,token_list,gender,pref_char):
+        preferences = []
+        in_tie = False
+        cur_set = set()
+        for token in token_list[1:]:
+            if token == '(':
+                if in_tie:
+                    raise NestedTiesError(gender, self.cur_line)
+                in_tie = True
+            elif token == ')':
+                if not in_tie:
+                    raise UnopenedTieError(gender, self.cur_line)
+                in_tie = False
+                preferences.append(cur_set.copy())
+                cur_set.clear()
+            else:
+                cur_set.add(pref_char+token)
+                if not in_tie:
+                    preferences.append(cur_set.copy())
+                    cur_set.clear()
+        if in_tie:
+            raise UnclosedTieError(gender, self.cur_line)
+        return preferences
+
     def _read_data(self) -> None:
         self.no_men = 0
         self.no_women = 0
         self.men = {}                
         self.women = {}
-        cur_line = 1
+        self.cur_line = 1
         
         with open(self.data, 'r') as file:
             file = file.read().splitlines()
@@ -27,36 +62,31 @@ class FileReader(AbstractReader):
 
         # build men dictionary
         for elt in file[1:self.no_men+1]:
-            cur_line += 1
-            entry = elt.split()
+            self.cur_line += 1
+            entry = self.regex_split(elt)
 
             if not entry or not entry[0].isdigit():
-                raise IDMisformatError("man", cur_line, line=True)
+                raise IDMisformatError("man", self.cur_line, line=True)
             man = f"m{entry[0]}"
             if man in self.men:
-                raise RepeatIDError("man", cur_line, line=True)
+                raise RepeatIDError("man", self.cur_line, line=True)
+            
+            # we don't check that every token is a digit
+            # this became unnecessary with the use of re.findall
 
-            for i in entry[1:]:
-                if not i.isdigit():
-                    raise PrefListMisformatError("man",cur_line,i,line=True)
-            preferences = [f"w{i}" for i in entry[1:]]
-
+            preferences = self._scan_preference_tokens(entry,"man","w")
             self.men[man] = {"list": preferences, "rank": {}}
 
         # build women dictionary
         for elt in file[self.no_men+1:self.no_men+self.no_women+1]:
-            cur_line += 1
-            entry = elt.split()
+            self.cur_line += 1
+            entry = self.regex_split(elt)
 
             if not entry or not entry[0].isdigit():
-                raise IDMisformatError("woman",cur_line,line=True)
+                raise IDMisformatError("woman",self.cur_line,line=True)
             woman = f"w{entry[0]}"
             if woman in self.women:
-                raise RepeatIDError("woman",cur_line,line=True)
+                raise RepeatIDError("woman",self.cur_line,line=True)
 
-            for i in entry[1:]:
-                if not i.isdigit():
-                    raise PrefListMisformatError("woman",cur_line,i,line=True)
-            preferences = [f"m{i}" for i in entry[1:]]
-
+            preferences = self._scan_preference_tokens(entry,"woman","m")
             self.women[woman] = {"list": preferences, "rank": {}}
