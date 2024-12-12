f942bc5aaaa03b67ae92daf21a7f322256846fbb
BenOnTheBoard <ben.pi.whitehead@gmail.com>
2024-12-12 20:01:19 +0000

    SPAS Verifier Set

---
+++ tests/SPATests/spasVerifier.py
@@ -0,0 +1,33 @@
+from algmatch.studentProjectAllocation import StudentProjectAllocation
+
+from tests.abstractTests.abstractVerifier import AbstractVerifier
+from tests.SPATests.utils.instanceGenerator import SPAInstanceGenerator
+from tests.SPATests.utils.minmaxSMs import MMSMS
+
+class SPASAbstractVerifier(AbstractVerifier):
+    def __init__(self, total_students,
+                 lower_project_bound, upper_project_bound):
+        """
+        It takes argument as follows (set in init):
+            number of men
+            number of women
+            lower bound of the preference list length
+            upper bound of the preference list length
+        """
+
+        self._total_students = total_students
+        self._lower_project_bound = lower_project_bound
+        self._upper_project_bound = upper_project_bound
+
+        generator_args = (total_students,
+                          lower_project_bound,
+                          upper_project_bound)
+
+        AbstractVerifier.__init__(
+            self,
+            StudentProjectAllocation,
+            ("students", "lecturers"),
+            SPAInstanceGenerator,
+            generator_args,
+            MMSMS
+        )
