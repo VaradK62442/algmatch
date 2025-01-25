2d9eaaf7c9417b87fe2b8cb8d7308bc18852c109
VaradK62442 <varadkulk123@gmail.com>
2025-01-25 17:23:39 +0000

    add SPA-P single and multiple class imports

--- src/algmatch/__init__.py
+++ src/algmatch/__init__.py
@@ -1,7 +1,11 @@
 from .hospitalResidentsProblem import HospitalResidentsProblem
 from .stableMarriageProblem import StableMarriageProblem
 from .studentProjectAllocation import StudentProjectAllocation
+from .studentProjectAllocationProjects import StudentProjectAllocationProjectsSingle
+from .studentProjectAllocationProjects import StudentProjectAllocationProjectsMultiple
 
 from .hospitalResidentsProblem import HospitalResidentsProblem as HR
 from .stableMarriageProblem import StableMarriageProblem as SM
 from .studentProjectAllocation import StudentProjectAllocation as SPA
+from .studentProjectAllocationProjects import StudentProjectAllocationProjectsSingle as SPAP_Single
+from .studentProjectAllocationProjects import StudentProjectAllocationProjectsMultiple as SPAP_Multiple
