89cf474d5f134c736b40ae0deb3a8b54b9885533
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-02-27 18:22:19 +0000

    Abstract of generic HR enumeration code.

--- tests/HRTests/utils/generic/hrGenericEnumerator.py
+++ tests/HRTests/utils/generic/hrGenericEnumerator.py
@@ -1,12 +1,5 @@
-from algmatch.stableMatchings.hospitalResidentsProblem.noTies.hrAbstract import (
-    HRAbstract,
-)
-
-
-class ESMS(HRAbstract):
-    def __init__(self, dictionary):
-        super(ESMS, self).__init__(dictionary=dictionary)
-
+class HRGenericEnumerator:
+    def __init__(self):
         self.M = {r: {"assigned": None} for r in self.residents} | {
             h: {"assigned": set()} for h in self.hospitals
         }
@@ -17,10 +10,13 @@ class ESMS(HRAbstract):
         # We cannot use 'sorted' without this key because that uses lexial order.
         self.resident_order_comparator = lambda r: int(r[1:])
 
-    def hospital_is_full(self, h):
+    def has_stability(self) -> bool:
+        raise NotImplementedError("Enumerators need to link to a stability definition.")
+
+    def hospital_is_full(self, h) -> bool:
         return self.hospitals[h]["capacity"] == len(self.M[h]["assigned"])
 
-    def save_matching(self):
+    def save_matching(self) -> None:
         stable_matching = {"resident_sided": {}, "hospital_sided": {}}
         for resident in self.residents:
             if self.M[resident]["assigned"] is None:
@@ -35,29 +31,27 @@ class ESMS(HRAbstract):
             )
         self.all_stable_matchings.append(stable_matching)
 
-    def add_pair(self, resident, hospital):
+    def add_pair(self, resident, hospital) -> None:
         self.M[resident]["assigned"] = hospital
         self.M[hospital]["assigned"].add(resident)
 
         if self.hospital_is_full(hospital):
             self.full_hospitals.add(hospital)
 
-    def delete_pair(self, resident, hospital):
+    def delete_pair(self, resident, hospital) -> None:
         self.M[resident]["assigned"] = None
         self.M[hospital]["assigned"].remove(resident)
         self.full_hospitals.discard(hospital)
 
-    def choose(self, i=1):
+    def choose(self, i=1) -> None:
         # if every resident is assigned
         if i > len(self.residents):
-            # if stable add to solutions list
-            if self._check_stability():
+            if self.has_stability():
                 self.save_matching()
 
         else:
             resident = "r" + str(i)
             for hospital in self.residents[resident]["list"]:
-                # avoid the over-filling of hospitals
                 if hospital not in self.full_hospitals:
                     self.add_pair(resident, hospital)
                     self.choose(i + 1)
@@ -66,5 +60,5 @@ class ESMS(HRAbstract):
             self.choose(i + 1)
 
     # alias with more readable name
-    def find_all_stable_matchings(self):
+    def find_all_stable_matchings(self) -> None:
         self.choose()
