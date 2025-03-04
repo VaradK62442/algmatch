37f1e133202c73db754350f759b3bebdf9a9a7d8
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-03-04 18:43:33 +0000

    Refactoring via AbstractTieGenerator
    Creation of this file allows us to avoid repetition across this class of generators.

--- tests/SMTests/utils/ties/smtInstanceGenerator.py
+++ tests/SMTests/utils/ties/smtInstanceGenerator.py
@@ -1,43 +1,30 @@
 import random
 
+from tests.abstractTestClasses.abstractTieGenerator import AbstractTieGenerator
 from tests.SMTests.utils.generic.smGenericGenerator import SMGenericGenerator
 
 
-class SMTInstanceGenerator(SMGenericGenerator):
+class SMTInstanceGenerator(SMGenericGenerator, AbstractTieGenerator):
     def __init__(self, men, women, lower_bound, upper_bound):
-        super().__init__(men, women, lower_bound, upper_bound)
-        self.tie_density = 0  # default to none
-
-    def set_tie_density(self, tie_density):
-        assert tie_density >= 0 and tie_density <= 1, "Tie density must be in [0,1]."
-        self.tie_density = tie_density
+        SMGenericGenerator.__init__(self, men, women, lower_bound, upper_bound)
+        AbstractTieGenerator.__init__()
 
     def _generate_men_lists(self):
         for man_list in self.instance["men"].values():
             random.shuffle(self.available_women)
-
             length = random.randint(self.li, self.lj)
-            if length == 0:
-                continue
 
-            man_list.append([self.available_women[0]])
-            for woman in self.available_men[1:length]:
-                if random.uniform(0, 1) < self.tie_density:
-                    man_list[-1].append(woman)
-                else:
-                    man_list.append([woman])
+            self._generate_tied_list(
+                man_list,
+                self.available_women[:length],
+            )
 
     def _generate_women_lists(self):
         for woman_list in self.instance["women"].values():
             random.shuffle(self.available_men)
-
             length = random.randint(self.li, self.lj)
-            if length == 0:
-                continue
 
-            woman_list.append([self.available_men[0]])
-            for man in self.available_men[1:length]:
-                if random.uniform(0, 1) < self.tie_density:
-                    woman_list[-1].append(man)
-                else:
-                    woman_list.append([man])
+            self._generate_tied_list(
+                woman_list,
+                self.available_men[:length],
+            )
