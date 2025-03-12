56778e72840f066a167c88abea6a54e5a07284fc
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-03-12 16:42:15 +0000

    Update abstract verifier to handle ties

--- tests/abstractTestClasses/abstractVerifier.py
+++ tests/abstractTestClasses/abstractVerifier.py
@@ -1,8 +1,9 @@
 class AbstractVerifier:
-    def __init__(self, problem, sides, gen, gen_args, brute_force):
+    def __init__(self, problem, sides, gen, gen_args, brute_force, stability_type=None):
         self.Problem = problem
         self.sides = sides
         self.BruteForce = brute_force
+        self.stability_type = stability_type
         self.gen = gen(*gen_args)
         self.current_instance = {}
 
@@ -11,14 +12,28 @@ class AbstractVerifier:
 
     def verify_instance(self):
         # optimal and pessimal from man/resident/student side
-
-        bruteforcer = self.BruteForce(dictionary=self.current_instance)
-        optimal_solver = self.Problem(
-            dictionary=self.current_instance, optimised_side=self.sides[0]
-        )
-        pessimal_solver = self.Problem(
-            dictionary=self.current_instance, optimised_side=self.sides[1]
-        )
+        if self.stability_type is not None:
+            bruteforcer = self.BruteForce(
+                dictionary=self.current_instance, stability_type=self.stability_type
+            )
+            optimal_solver = self.Problem(
+                dictionary=self.current_instance,
+                optimised_side=self.sides[0],
+                stability_type=self.stability_type,
+            )
+            pessimal_solver = self.Problem(
+                dictionary=self.current_instance,
+                optimised_side=self.sides[1],
+                stability_type=self.stability_type,
+            )
+        else:
+            bruteforcer = self.BruteForce(dictionary=self.current_instance)
+            optimal_solver = self.Problem(
+                dictionary=self.current_instance, optimised_side=self.sides[0]
+            )
+            pessimal_solver = self.Problem(
+                dictionary=self.current_instance, optimised_side=self.sides[1]
+            )
 
         bruteforcer.find_minmax_matchings()
         m_0 = optimal_solver.get_stable_matching()
