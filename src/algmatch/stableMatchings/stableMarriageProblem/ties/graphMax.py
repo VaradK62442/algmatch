ff10bf46b6064484d806acb36b08a479c814b3a6
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-03-21 21:56:12 +0000

    SMT Max-Matching Generator Bugfix

--- src/algmatch/stableMatchings/stableMarriageProblem/ties/graphMax.py
+++ src/algmatch/stableMatchings/stableMarriageProblem/ties/graphMax.py
@@ -7,13 +7,15 @@ class GraphMax:
         for v in self.G[u]["assigned"]:
             if not visited[v]:
                 visited[v] = True
-                if v not in self.matching or self.bpm(self.matching[v], visited):
+                if v not in self.matching or self.bpm(
+                    self.matching[v]["assigned"], visited
+                ):
                     self.matching[v] = {"assigned": u}
                     return
 
     def get_max_matching(self):
         self.matching = {}
-        for u in self.G:
+        for u in self.G.keys():
             visited = {v: False for v in self.G}
             self.bpm(u, visited)
 
