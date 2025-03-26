55050a0588eab2922d364f95a6f8e008142da729
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-03-26 00:51:28 +0000

    Moving setup steps to AbstractPrefenceInstance

--- src/algmatch/abstractClasses/abstractPreferenceInstance.py
+++ src/algmatch/abstractClasses/abstractPreferenceInstance.py
@@ -6,9 +6,15 @@ import os
 
 
 class AbstractPreferenceInstance:
-    def __init__(self, filename: str | None = None, dictionary: dict | None = None) -> None:
-        assert filename is not None or dictionary is not None, "Either filename or dictionary must be provided"
-        assert not (filename is not None and dictionary is not None), "Only one of filename or dictionary must be provided"
+    def __init__(
+        self, filename: str | None = None, dictionary: dict | None = None
+    ) -> None:
+        assert filename is not None or dictionary is not None, (
+            "Either filename or dictionary must be provided"
+        )
+        assert not (filename is not None and dictionary is not None), (
+            "Only one of filename or dictionary must be provided"
+        )
 
         if filename is not None:
             assert os.path.isfile(filename), f"File {filename} does not exist"
@@ -17,9 +23,21 @@ class AbstractPreferenceInstance:
         if dictionary is not None:
             self._load_from_dictionary(dictionary)
 
+        self.check_preference_lists()
+        self.clean_unacceptable_pairs()
+        self.set_up_rankings()
 
     def _load_from_file(self, filename: str) -> None:
         raise NotImplementedError("Method not implemented")
-    
+
     def _load_from_dictionary(self, dictionary: dict) -> None:
+        raise NotImplementedError("Method not implemented")
+
+    def check_preference_lists(self) -> None:
+        raise NotImplementedError("Method not implemented")
+
+    def clean_unacceptable_pairs(self) -> None:
+        raise NotImplementedError("Method not implemented")
+
+    def set_up_rankings(self) -> None:
         raise NotImplementedError("Method not implemented")
