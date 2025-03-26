48a728f3d18a8928d55ebc4fd106255a85f80866
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-03-26 00:51:29 +0000

    SM and HR PrefInst refactoring and formatting

--- src/algmatch/stableMatchings/stableMarriageProblem/smPreferenceInstance.py
+++ src/algmatch/stableMatchings/stableMarriageProblem/smPreferenceInstance.py
@@ -1,19 +1,24 @@
 """
 Store preference lists for Stable Marriage stable matching algorithm.
 """
+
 from itertools import product
 
-from algmatch.abstractClasses.abstractPreferenceInstance import AbstractPreferenceInstance
+from algmatch.abstractClasses.abstractPreferenceInstance import (
+    AbstractPreferenceInstance,
+)
 from algmatch.stableMatchings.stableMarriageProblem.fileReader import FileReader
-from algmatch.stableMatchings.stableMarriageProblem.dictionaryReader import DictionaryReader
+from algmatch.stableMatchings.stableMarriageProblem.dictionaryReader import (
+    DictionaryReader,
+)
 from algmatch.errors.InstanceSetupErrors import PrefRepError, PrefNotFoundError
 
+
 class SMPreferenceInstance(AbstractPreferenceInstance):
-    def __init__(self, filename: str | None = None, dictionary: dict | None = None) -> None:
+    def __init__(
+        self, filename: str | None = None, dictionary: dict | None = None
+    ) -> None:
         super().__init__(filename, dictionary)
-        self.check_preference_lists()
-        self.clean_unacceptable_pairs()
-        self.set_up_rankings()
 
     def _load_from_file(self, filename: str) -> None:
         reader = FileReader(filename)
@@ -27,22 +32,20 @@ class SMPreferenceInstance(AbstractPreferenceInstance):
 
     def check_preference_lists(self) -> None:
         for m, m_prefs in self.men.items():
-
             if len(set(m_prefs["list"])) != len(m_prefs["list"]):
-                raise PrefRepError("man",m)
-            
+                raise PrefRepError("man", m)
+
             for w in m_prefs["list"]:
                 if w not in self.women:
-                    raise PrefNotFoundError("man",m,w)
-            
-        for w, w_prefs in self.women.items():
+                    raise PrefNotFoundError("man", m, w)
 
+        for w, w_prefs in self.women.items():
             if len(set(w_prefs["list"])) != len(w_prefs["list"]):
-                raise PrefRepError("woman",w)
-            
+                raise PrefRepError("woman", w)
+
             for m in w_prefs["list"]:
                 if m not in self.men:
-                    raise PrefNotFoundError("woman",w,m)
+                    raise PrefNotFoundError("woman", w, m)
 
     def clean_unacceptable_pairs(self) -> None:
         for m, w in product(self.men, self.women):
@@ -58,6 +61,10 @@ class SMPreferenceInstance(AbstractPreferenceInstance):
 
     def set_up_rankings(self):
         for m in self.men:
-            self.men[m]["rank"] = {woman: idx for idx, woman in enumerate(self.men[m]["list"])}
+            self.men[m]["rank"] = {
+                woman: idx for idx, woman in enumerate(self.men[m]["list"])
+            }
         for w in self.women:
-            self.women[w]["rank"] = {man: idx for idx, man in enumerate(self.women[w]["list"])}
+            self.women[w]["rank"] = {
+                man: idx for idx, man in enumerate(self.women[w]["list"])
+            }
