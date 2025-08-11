c23b5a9a9a313cb8c796856e239b0131db1db8ed
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-08-11 02:18:29 +0100

    Expands HRT Super Single Verifier to test across a variety of tie frequencies evenly

--- tests/HRTests/HRTSuper/hrtSuperSingleVerifier.py
+++ tests/HRTests/HRTSuper/hrtSuperSingleVerifier.py
@@ -1,3 +1,4 @@
+import numpy as np
 from time import perf_counter_ns
 from tqdm import tqdm
 
@@ -28,19 +29,29 @@ class HRTSuperSingleVerifier(HRTSuperV, ASV):
 
 
 def main():
-    TOTAL_RESIDENTS = 12
-    TOTAL_HOSPITALS = 5
+    TOTAL_RESIDENTS = 5
+    TOTAL_HOSPITALS = 3
     LOWER_LIST_BOUND = 0
     UPPER_LIST_BOUND = 3
-    REPETITIONS = 80_000
+    TIE_DENSITY_STEPS = 10
+    REPS_PER_TDS = 5_000
+
+    td_step_size = 1 / TIE_DENSITY_STEPS
+    td_values = np.arange(0, 1 + td_step_size / 2, td_step_size)
 
     start = perf_counter_ns()
 
     verifier = HRTSuperSingleVerifier(
         TOTAL_RESIDENTS, TOTAL_HOSPITALS, LOWER_LIST_BOUND, UPPER_LIST_BOUND
     )
-    for _ in tqdm(range(REPETITIONS)):
-        verifier.run()
+
+    for td in td_values:
+        print("-" * 18)
+        print(f"With Tie Density: {td}")
+
+        verifier.gen.set_tie_density(td)
+        for _ in tqdm(range(REPS_PER_TDS)):
+            verifier.run()
 
     end = perf_counter_ns()
     print(f"\nFinal Runtime: {(end - start) / 1000**3}s")
